6286c9f49a28d67283dc042cc4df28366ac62059
wanieldilson <dan@00wilson.co.uk>
2022-11-11 09:53:25 +0000

    Add SSH script to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "dev-instance-ip" {
-    value = aws_instance.this.public_ip
+  value = aws_instance.this.public_ip
+}
+
+output "connection_script" {
+  value = templatefile("${var.host_os}-ssh-config.tpl", {
+    hostname = aws_instance.this.public_ip,
+    user     = "ubuntu",
+  identityfile = "~/.ssh/devInstanceKey" })
+
 }
