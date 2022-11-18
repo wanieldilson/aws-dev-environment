06885411f743930bd4253641e2f1ea6d07e6c557
wanieldilson <dan@00wilson.co.uk>
2022-11-18 09:08:21 +0000

    add role for Ec2 instance to assume

--- main.tf
+++ main.tf
@@ -13,12 +13,31 @@ module "compute" {
   instance_type     = "t2.micro"
 }
 
-module "dev-machine-2" {
-  source            = "./modules/compute"
-  security_group_id = module.networking.security_group_id
-  subnet_id         = module.networking.subnet_id
-  key_name          = "johnInstanceKey"
-  developer_name    = "john"
-  instance_type     = "t2.micro"
+data "aws_iam_policy_document" "inline_policy" {
+  statement {
+    actions   = ["*"]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "terraform-admin" {
+  name = "terraform-admin"
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+
+  inline_policy {
+    name = "administrator-access"
+    policy = data.aws_iam_policy_document.inline_policy.json
+  }
 
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "connection_script" {
   value = module.compute.connection_script
 }
-
-output "john_connection_script" {
-  value = module.dev-machine-2.connection_script
-}
