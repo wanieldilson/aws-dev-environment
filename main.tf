2a8a9fed2691151edc145eb8cbb595baf2b45c03
wanieldilson <dan@00wilson.co.uk>
2022-11-18 09:22:13 +0000

    :pencil2: fix missing instance reference

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ module "compute" {
   key_name             = "devInstanceKey"
   developer_name       = "wanieldilson"
   instance_type        = "t2.micro"
-  iam_instance_profile = aws_iam_instance_profile.terraform_profile
+  iam_instance_profile = aws_iam_instance_profile.terraform_profile.name
 }
 
