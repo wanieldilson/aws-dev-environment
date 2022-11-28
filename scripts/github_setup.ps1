8a044e820a2d7c887dfe11bf270f92c6bd3e2d88
wanieldilson <dan@00wilson.co.uk>
2022-11-28 14:00:51 +0000

    :hammer: make github script more user friendly

--- scripts/github_setup.ps1
+++ scripts/github_setup.ps1
@@ -1,8 +1,16 @@
 ## This script takes the first IP address in your SSH config and copies your SSH private key to it
-
+$ssh_key_name = Read-Host "Enter the name of your SSH key e.g awsDevInstanceKey"
 $ec2_instance_ip = Get-Content ~\.ssh\config | grep -o -m 1 '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
-Copy-Item ~\.ssh\id_ed25519 .
-scp -i ~\.ssh\id_ed25519.pub .\id_ed25519 ubuntu@${ec2_instance_ip}:/home/ubuntu/.ssh
-Remove-Item id_ed25519
+Copy-Item ~\.ssh\${ssh_key_name} .
+scp -i ~\.ssh\${ssh_key_name} .\${ssh_key_name} ubuntu@${ec2_instance_ip}:/home/ubuntu/.ssh
+scp -i ~\.ssh\${ssh_key_name} .\scripts\config ubuntu@${ec2_instance_ip}:/home/ubuntu/.ssh
+Remove-Item ${ssh_key_name} 
+$line = "=" * 85
 
-## Don't forget to chmod 600 your private key on the remote instance
+Write-Host $line -ForegroundColor Yellow
+Write-Host -NoNewline "Your SSH key ${ssh_key_name} has now been copied to your remote machine "
+Write-Host -ForegroundColor DarkGreen "${ec2_instance_ip}"
+Write-Host "On your remote machine please run the following command"
+Write-Host -ForegroundColor DarkGreen "chmod 600 ~/.ssh/${ssh_key_name}" 
+Write-Host $line -ForegroundColor Yellow
+
